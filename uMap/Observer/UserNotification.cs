62c6a1bc8c7044c7b270cdf52fa58fdc5fb0b274
Ledrunning <ledrunner@hotmail.com>
2019-10-11 09:51:02 +0300

    Major fixing

---
+++ uMap/Observer/UserNotification.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace uMap.Project.Observer
+{
+    public class UserNotification
+    {
+        private string message;
+
+        public UserNotification()
+        {
+        }
+
+        public UserNotification(string message)
+        {
+            this.message = message;
+        }
+
+        public void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Упс!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
