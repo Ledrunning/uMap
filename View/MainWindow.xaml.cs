46be1fbc2778a18b2dacf84ed91d6d124b48e8d9
omazinov <osman.mazinov@arsis.ru>
2019-10-11 08:24:40 +0300

    Добавьте файлы проекта.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Map;
+
+namespace Arsis.MapUtility.View
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
